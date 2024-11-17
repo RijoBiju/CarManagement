47c0df45fc436c223c8f97995f54af68cc9b428a
Rijo Biju <rijobijua.github@outlook.com>
2024-11-17 22:28:12 +0530

    expense routes setup in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const carRoutes = require("./routes/carRoutes");
 const expenseTypeRoutes = require("./routes/expenseTypeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use("/api", carRoutes);
 app.use("/api", expenseTypeRoutes);
+app.use("/api", expenseRoutes);
 
 app.use(errorHandler);
 
