47c22e93788ea793b82e317ccafcb537e6eebc17
Akhil Vengassery <akhillav@gmail.com>
2025-01-31 22:51:29 +0530

    change port

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const connectDB = require("./utils/db");
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
