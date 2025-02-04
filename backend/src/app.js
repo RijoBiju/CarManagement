9968adc595ad3d0b6dea7da01eef4c222117a827
Akhil Vengassery <akhillav@gmail.com>
2025-02-04 11:16:42 +0530

    pck.jsno

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,11 @@ const expenseRoutes = require("./routes/expenseRoutes");
 
 const errorHandler = require("./middleware/errorHandler");
 
+console.log(
+  "Running in: ",
+  process.env.NODE_ENV ? process.env.NODE_ENV : "development"
+);
+
 const app = express();
 app.use(express.json());
 app.use(cors());
